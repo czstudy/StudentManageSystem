99ddc606a95ffa04e1a535e3baa8e97fcc21abc7
Administrator <Administrator@WIN7-20190814ID>
2019-09-17 08:49:33 +0800

    first sersion

---
+++ ScoreManageSystem/test/com/dao/impl/TeacherDAOMysqlImplTest.java
@@ -0,0 +1,39 @@
+package com.dao.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.dto.Teacher;
+
+public class TeacherDAOMysqlImplTest {
+
+	private TeacherDAOMysqlImpl teacherDAOMysqlImpl;
+	@Before
+	public void setUp() throws Exception {
+		teacherDAOMysqlImpl=new TeacherDAOMysqlImpl();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testLogin() {
+//		fail("Not yet implemented");
+		Teacher teacher=null;
+		try {
+			teacher=teacherDAOMysqlImpl.login("001", "321");
+			if(teacher!=null){
+				//System.out.println("name:"+teacher.getName());
+			}
+			assertEquals("tom", teacher.getName());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
